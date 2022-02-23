cc551dd36c4decd7ec2ba5329a2950ffb35dcfac
kunalsonawane730 <95461743+kunalsonawane730@users.noreply.github.com>
2022-02-24 01:08:26 +0530

    [kunal]commiting changes for creating BST and add element program

---
+++ BinarySearchTree/BinarySearchTree/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int Element { get; set; }
+        public Node Leftside { get; set; }
+        public Node Rightside { get; set; }
+
+        public Node(int data)
+        {
+            this.Element = data;
+        }
+    }
+}
